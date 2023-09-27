4eee215480ce294f5d7325982c922169951edbb7
varbogas <varbogas@gmail.com>
2023-09-27 11:20:01 -0700

    group of cubes

--- script.js
+++ script.js
@@ -7,10 +7,34 @@ const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
 //Objects
- const geometry = new THREE.BoxGeometry(1, 1, 1)
- const material = new THREE.MeshBasicMaterial({ color: 0x73C2FB })
- const mesh = new THREE.Mesh(geometry, material)
- scene.add(mesh)
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
 
  //Sizes 
  const sizes = {
