f9a1e1a15d2a6dc025187b65f8c2721f03fc07b4
varbogas <varbogas@gmail.com>
2023-09-27 14:55:53 -0700

    animation solution 1

--- script.js
+++ script.js
@@ -6,35 +6,11 @@ const canvas = document.querySelector('.webgl')
 //Scene
 const scene = new THREE.Scene()
 
-//Objects
-/**
- * Objects
- */
-const group = new THREE.Group()
-group.scale.y = 2
-group.rotation.y = 0.2
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube1.position.x = - 1.5
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube2.position.x = 0
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube3.position.x = 1.5
-group.add(cube3)
+//Object
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
  //Sizes 
  const sizes = {
@@ -53,4 +29,27 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Time
+
+let time = Date.now()
+
+//Animations
+
+const tick = () =>
+{
+    //Time
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+    //Update objects
+    mesh.rotation.y += 0.001 * deltaTime
+
+    //Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
